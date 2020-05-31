3300f4734492582ad78c91dbd2d497a086405c6c
Suryendu <Suryendbbhattacharyya@gmail.com>
2020-05-31 22:18:41 +0530

    second commit

--- Dump-OUStructure.ps1
+++ Dump-OUStructure.ps1
@@ -1,4 +1,4 @@
-function Get-ADOUhirearchy ([string]$dn, $level = 1)
+function Get-ADOUHirearchy ([string]$dn, $level = 1)
 {
     if ($level -eq 1) { $dn }
     Get-ADOrganizationalUnit -filter * -SearchBase $dn -SearchScope OneLevel | 
@@ -6,8 +6,8 @@ function Get-ADOUhirearchy ([string]$dn, $level = 1)
         ForEach-Object {
             $components = ($_.distinguishedname).split(',')
             "$('--' * $level) $($components[0])"
-            Recurse-OU -dn $_.distinguishedname -level ($level+1)
+            Get-ADOUHirearchy -dn $_.distinguishedname -level ($level+1)
         }
 }
 
-Get-ADOUhirearchy -dn (Get-ADDomain).DistinguishedName
+Get-ADOUHirearchy -dn (Get-ADDomain).DistinguishedName
