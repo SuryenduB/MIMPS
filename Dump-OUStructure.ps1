cd698fe91569819ccb52ed9cb305eb4e62487c14
Suryendu <Suryendbbhattacharyya@gmail.com>
2020-06-01 00:45:03 +0530

    add comment

---
+++ Dump-OUStructure.ps1
@@ -0,0 +1,16 @@
+
+#Dummp OU Structure
+# Recursive function is a function that calls itself
+function Get-ADOUHirearchy ([string]$dn, $level = 1)
+{
+    if ($level -eq 1) { $dn }
+    Get-ADOrganizationalUnit -filter * -SearchBase $dn -SearchScope OneLevel | 
+        Sort-Object -Property distinguishedName | 
+        ForEach-Object {
+            $components = ($_.distinguishedname).split(',')
+            "$('--' * $level) $($components[0])"
+            Get-ADOUHirearchy -dn $_.distinguishedname -level ($level+1)
+        }
+}
+
+Get-ADOUHirearchy -dn (Get-ADDomain).DistinguishedName
